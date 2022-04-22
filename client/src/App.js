95be46887d7404ea7e53309da0ee3c21f0427561
Matias Guerrero <matias.f.guerrero19@gmail.com>
2022-04-22 15:19:30 -0300

    The method update implemented at the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import Preloader from "./components/Preloader";
-import { createTodo, readTodos } from "./functions";
+import { createTodo, readTodos, updateTodo } from "./functions";
 
 function App() {
   const [todo, setTodo] = useState({ title: '', content: '' });
@@ -18,7 +18,7 @@ function App() {
       setTodos(result)
     }
     fetchData()
-  }, []);
+  }, [currentId]);
 
   const clear = () => {
     setCurrentId(0)
@@ -37,8 +37,14 @@ function App() {
 
   const onSubmitHandler = async (e) => {
     e.preventDefault();
-    const result = await createTodo(todo)
-    setTodos([...todos, result])
+    if(currentId === 0) {
+      const result = await createTodo(todo)
+      setTodos([...todos, result])
+      clear();
+    } else {
+      await updateTodo(currentId, todo)
+      clear();
+    }
     // console.log(result)
   }
 

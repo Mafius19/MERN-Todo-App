478a1d4a5f2fd27ef911dbd1ae18f02632ab9579
Matias Guerrero <matias.f.guerrero19@gmail.com>
2022-04-22 12:14:23 -0300

    List in app.js the elements of todos and setting the preloader accordingly

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,17 @@ import Preloader from "./components/Preloader";
 import { createTodo, readTodos } from "./functions";
 
 function App() {
-  const [todo, setTodo] = useState({title:'', content:''});
+  const [todo, setTodo] = useState({ title: '', content: '' });
+  const [todos, setTodos] = useState(null);
   useEffect(() => {
-    const fetchData = async() => {
+    const fetchData = async () => {
       const result = await readTodos();
-      console.log(result);
+      setTodos(result)
     }
     fetchData()
   }, []);
 
-  const onSubmitHandler = async(e) => {
+  const onSubmitHandler = async (e) => {
     e.preventDefault();
     const result = await createTodo(todo)
     console.log(result)
@@ -21,17 +22,17 @@ function App() {
   return (
     <div className="container">
       <div className="row">
-        <pre>{JSON.stringify(todo)}</pre>
+        <pre>{JSON.stringify(todos)}</pre>
         <form className="col s12" onSubmit={onSubmitHandler}>
           <div className="row">
             <div className="input-field col s6">
               <i className="material-icons prefix">title</i>
-              <input id="icon_prefix" type="text" className="validate" onChange={e=>setTodo({...todo, title: e.target.value})} />
+              <input id="icon_prefix" type="text" className="validate" onChange={e => setTodo({ ...todo, title: e.target.value })} />
               <label htmlFor="title">Title</label>
             </div>
             <div className="input-field col s6">
               <i className="material-icons prefix">description</i>
-              <input id="description" type="tel" className="validate" onChange={e=>setTodo({...todo, content: e.target.value})}/>
+              <input id="description" type="tel" className="validate" onChange={e => setTodo({ ...todo, content: e.target.value })} />
               <label htmlFor="Content">Content</label>
             </div>
           </div>
@@ -39,13 +40,28 @@ function App() {
             <button className="waves-effect waves-light btn">Submit</button>
           </div>
         </form>
-        <Preloader />
-        <div class="collection">
-          <a href="#!" class="collection-item">Alvin</a>
-          <a href="#!" class="collection-item active">Alvin</a>
-          <a href="#!" class="collection-item">Alvin</a>
-          <a href="#!" class="collection-item">Alvin</a>
-        </div>
+        {
+          !todos ? <Preloader /> : todos.length > 0 ?
+            <ul className="collection">
+              {todos.map(item => (
+                <li key={item._id} className="collection-item">
+                  <div>
+                    <h5>{item.title}</h5>
+                    <p> {item.content}
+                    <a href="#!" className="secondary-content">
+                      <i className="material-icons">delete</i>
+                    </a>
+                    </p>
+                  </div>
+                </li>
+
+              ))
+              }
+            </ul>
+            :
+            <div><h5>Nothing to do</h5></div>
+        }
+
       </div>
     </div>
   );
